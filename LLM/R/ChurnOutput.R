d6511b88ef2f2d769388a55a23e20d1f987e9bc1
sharanyakamath <sherrykamath@gmail.com>
2019-11-05 14:27:19 +0530

    confusion matrix

--- LLM/R/ChurnOutput.R
+++ LLM/R/ChurnOutput.R
@@ -25,11 +25,14 @@ nrow(ChurnTrain)
 # print(ChurnTrain[,20])
 # write.csv(ChurnTrain[,"Churn"],"try.csv")
 # Create the LLM
-Churn.llm <- llm(X = ChurnTrain[,-c(20)],Y = ChurnTrain$Churn, threshold_pruning = 0.25, nbr_obs_leaf = 100)
+Churn.llm <- llm.cv(X = ChurnTrain[,-c(20)],Y = ChurnTrain$Churn, cv=4, threshold_pruning = 0.25, nbr_obs_leaf = 100)
 
 ## Use the model on the test dataset to make a prediction
-ChurnPrediction <- predict.llm(object = Churn.llm, X = ChurnTest[,-c(20)])
+# ChurnPrediction <- predict.llm(object = Churn.llm, X = ChurnTest[,-c(20)])
 # ## Optionally add the dependent to calculate performance statistics such as AUC
-PimaPrediction <- cbind(ChurnPrediction, "Churn" = ChurnTest[,"Churn"])
+# ChurnPrediction <- cbind(ChurnPrediction, "Churn" = ChurnTest[,"Churn"])
 
-print(ChurnPrediction)
+# Churn.Viz <- table.llm.html(object = Churn.llm, headertext = "This is an example of the LLM model",
+# footertext = "Enjoy the package!")
+# write(Churn.Viz, "Churn.html")
+print(Churn.llm)
