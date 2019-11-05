794d1fcbaaf21df37fcd5d153da3810c17895099
sharanyakamath <sherrykamath@gmail.com>
2019-11-05 14:59:45 +0530

    predict

--- LLM/R/ChurnOutput.R
+++ LLM/R/ChurnOutput.R
@@ -25,14 +25,18 @@ nrow(ChurnTrain)
 # print(ChurnTrain[,20])
 # write.csv(ChurnTrain[,"Churn"],"try.csv")
 # Create the LLM
-Churn.llm <- llm.cv(X = ChurnTrain[,-c(20)],Y = ChurnTrain$Churn, cv=4, threshold_pruning = 0.25, nbr_obs_leaf = 100)
+Churn.llm <- llm(X = ChurnTrain[,-c(20)],Y = ChurnTrain$Churn, threshold_pruning = 0.25, nbr_obs_leaf = 100)
+# Churn.llm.cv <- llm.cv(X = ChurnTrain[,-c(20)],Y = ChurnTrain$Churn, cv=4, threshold_pruning = 0.25, nbr_obs_leaf = 100)
 
 ## Use the model on the test dataset to make a prediction
 # ChurnPrediction <- predict.llm(object = Churn.llm, X = ChurnTest[,-c(20)])
-# ## Optionally add the dependent to calculate performance statistics such as AUC
-# ChurnPrediction <- cbind(ChurnPrediction, "Churn" = ChurnTest[,"Churn"])
+ChurnPrediction <- predict.llm(object = Churn.llm, X = ChurnTest[,-c(20)])
 
-# Churn.Viz <- table.llm.html(object = Churn.llm, headertext = "This is an example of the LLM model",
-# footertext = "Enjoy the package!")
+# ## Optionally add the dependent to calculate performance statistics such as AUC
+ChurnPrediction <- cbind(ChurnPrediction, "Churn" = ChurnTest[,"Churn"])
+
+# Churn.Viz <- table.llm.html(object = Churn.llm, headertext = "This is an example of the LLM model")
 # write(Churn.Viz, "Churn.html")
-print(Churn.llm)
+# print(ChurnPrediction)
+
+write.csv(ChurnPrediction,"ChurnPrdiction.csv")
