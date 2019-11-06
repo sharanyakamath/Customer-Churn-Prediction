67bbede2392cee3058a4f401b675fa73d10a1d64
sharanyakamath <sherrykamath@gmail.com>
2019-11-06 22:35:46 +0530

    AUC plot

--- LLM/R/llm.R
+++ LLM/R/llm.R
@@ -185,11 +185,14 @@ llm <- function(X,Y,threshold_pruning=0.25 , nbr_obs_leaf=100) {
 
     # Train a LR for each subset with forward variable selection
     # build a glm model on the training data
-    LR <- stats::glm(y_sel ~ ., data=train_ss[,1:(ncol(basetabadc150392)-1)], family=stats::binomial("logit"))
-    LR1 <- stats::glm(y_sel ~ 1, data=train_ss[,1:(ncol(basetabadc150392)-1)], family=stats::binomial("logit"))
+    # LR <- stats::glm(y_sel ~ ., data=train_ss[,1:(ncol(basetabadc150392)-1)], family=stats::binomial("logit"))
+    # LR1 <- stats::glm(y_sel ~ 1, data=train_ss[,1:(ncol(basetabadc150392)-1)], family=stats::binomial("logit"))
+    RF = randomForest(x=train_ss[,1:(ncol(basetabadc150392)-1)], y=y_sel ~ .)
+    RF1 = randomForest(x=train_ss[,1:(ncol(basetabadc150392)-1)], y=y_sel ~ .)
 
     # stepwise variable selection
-    listythelist[[l]] <- stats::step(LR1,direction="forward" ,scope = list(lower= LR1, upper = LR), trace = 0)
+    # listythelist[[l]] <- stats::step(LR1,direction="forward" ,scope = list(lower= LR1, upper = LR), trace = 0)
+    listythelist[[l]] <- stats::step(RF1,direction="forward" ,scope = list(lower= RF1, upper = RF), trace = 0)
 
   }
   myreturn[[1]] <- newvar
@@ -200,15 +203,15 @@ llm <- function(X,Y,threshold_pruning=0.25 , nbr_obs_leaf=100) {
   return(myreturn)
 }
 
-# Test
-if (requireNamespace("mlbench", quietly = TRUE)) {
-  library("mlbench")
-}
-data("PimaIndiansDiabetes")
-## Split in training and test (2/3 - 1/3)
-idtrain <- c(sample(1:768,512))
-PimaTrain <-PimaIndiansDiabetes[idtrain,]
-Pimatest <-PimaIndiansDiabetes[-idtrain,]
-## Create the LLM
-Pima.llm <- llm(X = PimaTrain[,-c(9)],Y = PimaTrain$diabetes,
-threshold_pruning = 0.25,nbr_obs_leaf = 100)
+# # Test
+# if (requireNamespace("mlbench", quietly = TRUE)) {
+#   library("mlbench")
+# }
+# data("PimaIndiansDiabetes")
+# ## Split in training and test (2/3 - 1/3)
+# idtrain <- c(sample(1:768,512))
+# PimaTrain <-PimaIndiansDiabetes[idtrain,]
+# Pimatest <-PimaIndiansDiabetes[-idtrain,]
+# ## Create the LLM
+# Pima.llm <- llm(X = PimaTrain[,-c(9)],Y = PimaTrain$diabetes,
+# threshold_pruning = 0.25,nbr_obs_leaf = 100)
